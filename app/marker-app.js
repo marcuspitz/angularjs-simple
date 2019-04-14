bd1e3d5f47ad24f0236d492effab6ea1b56897ea
Marcus <marcusviniciuspitz@gmail.com>
2019-04-14 12:23:33 -0300

    Just managing simple states in Angular

--- app/marker-app.js
+++ app/marker-app.js
@@ -13,19 +13,54 @@ angular.module('Marker', [
     $scope.bookmarks = [
         {"id": 0, "title": "C++", "url": "www.google.com.br", "category": "Development"},
         {"id": 1, "title": "Angular", "url": "www.google.com.br", "category": "Development"},
-        {"id": 2, "title": "SQL Server", "url": "www.google.com.br", "category": "Development"}
+        {"id": 2, "title": "SQL Server", "url": "www.google.com.br", "category": "Development"},
+        {"id": 3, "title": "Arquiteto", "url": "www.google.com.br", "category": "Design"},
+        {"id": 4, "title": "Working out", "url": "www.google.com.br", "category": "Exercise"},
+        {"id": 5, "title": "Jokes", "url": "www.google.com.br", "category": "Humor"}
     ];
 
 
     $scope.currentCategory = null;
     $scope.setCurrentCategory = (category) => {
-        console.log('testesd00');
-        this.currentCategory = category;
-    };
-    
-    $scope.isCurrentCategory = (category) => {
-        return this.currentCategory != null && this.currentCategory.name === category.name;
+        $scope.currentCategory = category;
+        $scope.cancelCreating();
+        $scope.cancelEditing();
     };
 
+    $scope.isCurrentCategory = (category) => {
+        return $scope.currentCategory != null && $scope.currentCategory.name === category.name;
+    };
+
+    /**
+     * todo it's a sample needing improvements
+     */
+    $scope.isCreating = false;
+    $scope.isEditing = false;
+
+    $scope.startCreating = () => {
+        $scope.isCreating = true;
+        $scope.isEditing = false;
+    };
+
+    $scope.cancelCreating = () => {
+        $scope.isCreating = false;
+    };
+
+    $scope.startEditing = () => {
+        $scope.isCreating = false;
+        $scope.isEditing = true;
+    };
+
+    $scope.cancelEditing = () => {
+        $scope.isEditing = false;
+    };
+
+    $scope.shouldShowCreating = () => {
+        return $scope.currentCategory && !$scope.isEditing;
+    }
+
+    $scope.shouldShowEditing = () => {
+        return $scope.isEditing && !$scope.isCreating;
+    }
 
 });
